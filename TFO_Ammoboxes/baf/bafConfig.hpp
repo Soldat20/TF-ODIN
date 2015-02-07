52694bde03765620f997f9cb581d3f0ac714b3f1
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 02:35:29 -0500

    Update bafConfig.hpp

--- TFO_Ammoboxes/baf/bafConfig.hpp
+++ TFO_Ammoboxes/baf/bafConfig.hpp
@@ -11,7 +11,51 @@
 		SLX_XEH_DISABLED = 0;
 		
 		class TransportBackpacks {};		
-		class TransportItems {};
+		class TransportItems {
+			
+		// ACCESSORIES
+			class _xx_ItemWatch {
+				count = 50;
+				name = "ItemWatch";
+			};
+			class _xx_ItemCompass {
+				count = 50;
+				name = "ItemCompass";
+			};
+			class _xx_ItemGPS {
+				count = 50;
+				name = "ItemGPS";
+			};
+			class _xx_ItemRadio {
+				count = 50;
+				name = "ItemRadio";
+			};
+			class _xx_ItemMap {
+				count = 50;
+				name = "ItemMap";
+			};
+			class _xx_Binocular {
+				count = 50;
+				name = "Binocular";
+			};
+			class _xx_Rangefinder {
+				count = 50;
+				name = "Rangefinder";
+			};
+			class _xx_itemandroid {
+				count = 50;
+				name = "itemandroid";
+			};
+			
+			// HELMETS
+
+			class _xx_H_mas_uk_beret {
+				count = 100;
+				name = "H_mas_uk_beret";
+			};		
+			
+			
+		};
 		class TransportMagazines {};		
 		class TransportWeapons {};
 	};
