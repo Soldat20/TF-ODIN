888ec0474fa4f89649979f51cf77f5fb3dc4e294
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 02:28:17 -0500

    Update gui_editor_fmt.hpp

--- TFO_Main/lomgr/gui_editor_fmt.hpp
+++ TFO_Main/lomgr/gui_editor_fmt.hpp
@@ -3,7 +3,7 @@ $[
 	1.063,
 	["loadoutmgr",[[0,0,1,1],0.025,0.04,"GUI_GRID"],3,0,0],
 	[2200,"background",[2,"",["-3.5 * GUI_GRID_W + GUI_GRID_X","2.5 * GUI_GRID_H + GUI_GRID_Y","47.5 * GUI_GRID_W","19.5 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
-	[1800,"frame",[2,"MEB Loadout Manager",["-3 * GUI_GRID_W + GUI_GRID_X","3 * GUI_GRID_H + GUI_GRID_Y","46.5079 * GUI_GRID_W","18.5873 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1800,"frame",[2,"TFO Loadout Manager",["-3 * GUI_GRID_W + GUI_GRID_X","3 * GUI_GRID_H + GUI_GRID_Y","46.5079 * GUI_GRID_W","18.5873 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
 	[1401,"saveBox",[2,"Save Name",["-2 * GUI_GRID_W + GUI_GRID_X","4 * GUI_GRID_H + GUI_GRID_Y","13.5 * GUI_GRID_W","1.5 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
 	[1500,"loLIst",[2,"",["-1.88 * GUI_GRID_W + GUI_GRID_X","5.95 * GUI_GRID_H + GUI_GRID_Y","13.2619 * GUI_GRID_W","14.996 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
 	[2400,"saveBtn",[2,"Save",["12.8 * GUI_GRID_W + GUI_GRID_X","4.01 * GUI_GRID_H + GUI_GRID_Y","7 * GUI_GRID_W","1.5 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
@@ -15,5 +15,3 @@ $[
 	[2500,"",[2,"",["12.8 * GUI_GRID_W + GUI_GRID_X","11.47 * GUI_GRID_H + GUI_GRID_Y","6.77778 * GUI_GRID_W","1.5 * GUI_GRID_H"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]]
 ]
 */
-
-
