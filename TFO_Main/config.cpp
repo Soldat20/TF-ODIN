f99c7e09770eff8769077497bcbbb313762c19fb
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 02:04:20 -0500

    Create config.cpp

---
+++ TFO_Main/config.cpp
@@ -0,0 +1,93 @@
+#define PREFIX meb_main
+#define COMPONENT main
+#define MAJOR 1
+#define MINOR 0
+#define PATCHLVL 0
+#define BUILD 0
+#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
+#define REQUIRED_VERSION 1.0
+#include "\x\cba\addons\main\script_macros_common.hpp"
+
+
+class CfgPatches
+{
+	class PREFIX  // meb
+		{
+			units[] = {};
+			weapons[] = {};
+			requiredVersion = REQUIRED_VERSION;
+			requiredAddons[] = {"CBA_MAIN"};
+			versionDesc = "11th MEU(SOC) Ammoboxes";
+			versionAct = "";
+			VERSION_CONFIG;
+			author[] = {"11th MEU(SOC) Mod Team"};
+			authorUrl = "http://www.11thmeu.org";
+		};
+};
+
+class CfgSettings 
+{
+	class CBA 
+	{
+		class Versioning 
+		{
+			class PREFIX 
+			{
+				class Dependencies 
+				{
+					CBA[] = {"cba_main", { 1,0,0 },"true"};
+				};
+			};
+		};
+	};
+};
+
+class CfgMods {
+	class PREFIX {
+		dir = "@MARSOC_Boxes";
+		name = "11th MEU(SOC) MOD Pack";
+		picture = "";
+		hidePicture = "True";
+		hideName = "False";
+		actionName = "Website";
+		action = "http://www.11thmeu.org";
+		description = "";
+	};
+};
+class Extended_PostInit_EventHandlers
+{
+	class PREFIX
+	{
+		clientInit = QUOTE(call COMPILE_FILE2(\meb_main\player_init.sqf));
+
+		//serverInit = QUOTE(call COMPILE_FILE2(\meb_main\server_init.sqf));
+	};
+};
+
+
+
+class CfgWeapons
+{
+	
+};
+
+class CfgNotifications
+{
+	
+
+};
+
+
+
+
+class CfgVehicles
+{
+
+	
+};
+
+
+#include <\meb_main\lomgr\meb_gui_parents.hpp>
+
+#include <\meb_main\lomgr\loadoutmgr_gui.hpp>
