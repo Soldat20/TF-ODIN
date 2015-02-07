8db01a79244d1cfd3788567709b00a968ad2a00b
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 02:52:39 -0500

    Added CSE Medical Items

--- TFO_Ammoboxes/baf/bafConfig.hpp
+++ TFO_Ammoboxes/baf/bafConfig.hpp
@@ -54,7 +54,120 @@
 				name = "H_mas_uk_beret";
 			};		
 			
-			
+			// Medical
+
+			class _xx_cse_bandage_basic {
+				count = 500;
+				name = "cse_bandage_basic";
+			};
+			class _xx_cse_packing_bandage  {
+					count = 500;
+					name = "cse_packing_bandage";
+			};
+			class _xx_cse_tourniquet {
+					count = 500;
+					name = "cse_tourniquet";
+			};
+			class _xx_cse_splint {
+					count = 500;
+					name = "cse_splint";
+			};
+			class _xx_cse_morphine {
+					count = 500;
+					name = "cse_morphine";
+			};
+			class _xx_cse_atropine {
+					count = 500;
+					name = "cse_atropine";
+			};
+			class _xx_cse_epinephrine {
+					count = 500;
+					name = "cse_epinephrine";
+			};
+			class _xx_cse_plasma_iv {
+					count = 500;
+					name = "cse_plasma_iv";
+			};
+			class _xx_cse_plasma_iv_500 {
+					count = 500;
+					name = "cse_plasma_iv_500";
+			};
+			class _xx_cse_plasma_iv_250 {
+					count = 500;
+					name = "cse_plasma_iv_250";
+			};
+			class _xx_cse_blood_iv {
+					count = 500;
+					name = "cse_blood_iv";
+			};
+			class _xx_cse_blood_iv_500 {
+					count = 500;
+					name = "cse_blood_iv_500";
+			};
+			class _xx_cse_blood_iv_250 {
+					count = 500;
+					name = "cse_blood_iv_250";
+			};
+			class _xx_cse_saline_iv {
+					count = 500;
+					name = "cse_saline_iv";
+			};
+			class _xx_cse_saline_iv_500 {
+					count = 500;
+					name = "cse_saline_iv_500";
+			};
+			class _xx_cse_saline_iv_250 {
+					count = 500;
+					name = "cse_saline_iv_250";
+			};
+			class _xx_cse_quikclot {
+					count = 500;
+					name = "cse_quikclot";
+			};
+			class _xx_cse_nasopharyngeal_tube {
+					count = 500;
+					name = "cse_nasopharyngeal_tube";
+			};
+			class _xx_cse_opa {
+					count = 500;
+					name = "cse_opa";
+			};
+			class _xx_cse_bandageElastic {
+					count = 500;
+					name = "cse_bandageElastic";
+			};
+			class _xx_cse_liquidSkin {
+					count = 500;
+					name = "cse_liquidSkin";
+			};
+			class _xx_cse_chestseal {
+					count = 500;
+					name = "cse_chestseal";
+			};
+			class _xx_cse_personal_aid_kit {
+					count = 500;
+					name = "cse_personal_aid_kit";
+			};
+			class _xx_cse_itemBodyBag {
+					count = 500;
+					name = "cse_itemBodyBag";
+			};
+			class _xx_cse_surgical_kit {
+					count = 500;
+					name = "cse_surgical_kit";
+			};
+			class _xx_cse_earplugs {
+					count = 500;
+					name = "cse_earplugs";
+			};
+			class _xx_cse_earplugs_electronic {
+					count = 500;
+					name = "cse_earplugs_electronic";
+			};
+			class _xx_cse_Keycuffs {
+					count = 500;
+					name = "cse_Keycuffs";
+			};
 		};
 		class TransportMagazines {};		
 		class TransportWeapons {};
