b5e792278ed9a28a950ffc8de3016d0496d13d43
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 02:02:17 -0500

    Create bafConfig.hpp

---
+++ TFO_Ammoboxes/baf/bafConfig.hpp
@@ -0,0 +1,17 @@
+// class CfgVehicles
+// {
+
+	class B_CargoNet_01_ammo_F;
+	class Box_tfo_baf : B_CargoNet_01_ammo_F {
+		displayname = "[TFO] UKSF Box";
+		maximumLoad = 999999;
+		transportmaxmagazines = 999999;
+		transportmaxweapons = 999999;
+		transportMaxBackpacks = 999999;
+		SLX_XEH_DISABLED = 0;
+		
+		class TransportBackpacks {};		
+		class TransportItems {};
+		class TransportMagazines {};		
+		class TransportWeapons {};
+	};
