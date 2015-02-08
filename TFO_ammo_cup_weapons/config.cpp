21a9b413107e13c045484e57844546a6f666129c
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 21:53:53 -0500

    Update config.cpp

--- TFO_ammo_cup_weapons/config.cpp
+++ TFO_ammo_cup_weapons/config.cpp
@@ -14,14 +14,14 @@ class CfgWeapons {
 	class Rifle_Base_F;	// External class reference
 	class Rifle_Long_Base_F; // External Class reference
 	
-	class CUP_arifle_L85A2 : CUP_arifle_L85A2_Base {
+	class CUP_arifle_L85A2_Base : Rifle_Base_F {
 		magazines[] = {"rhs_mag_30Rnd_556x45_Mk318_Stanag","rhs_mag_30Rnd_556x45_Mk262_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow"};
 	};
-	class CUP_arifle_L85A2_GL : CUP_arifle_L85A2_Base {
+	class CUP_arifle_L86A2_Base : Rifle_Long_Base_F {
 		magazines[] = {"rhs_mag_30Rnd_556x45_Mk318_Stanag","rhs_mag_30Rnd_556x45_Mk262_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow"};
 	};
-	class CUP_arifle_L86A2 : CUP_arifle_L86A2_Base {
-		magazines[] = {"rhs_mag_30Rnd_556x45_Mk318_Stanag","rhs_mag_30Rnd_556x45_Mk262_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow"};
+	class CUP_lmg_L110A1 : Rifle_Long_Base_F {
+		magazines[] = {"rhsusf_100Rnd_556x45_soft_pouch", "rhs_mag_30Rnd_556x45_Mk318_Stanag","rhs_mag_30Rnd_556x45_Mk262_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow"};
 	};
 	class c7_base : Rifle_Base_F {
 		magazines[] = {"rhs_mag_30Rnd_556x45_Mk318_Stanag","rhs_mag_30Rnd_556x45_Mk262_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow"};
