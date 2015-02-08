c5c1ef8abf163cc6e24289e4b3ea14ec88fda98c
Soldat20 <chrismcwilliams89@gmail.com>
2015-02-07 21:16:00 -0500

    Update config.cpp

--- TFO_Boxes/config.cpp
+++ TFO_Boxes/config.cpp
@@ -11,7 +11,7 @@
 
 class CfgPatches {
 	class tfo_boxes {
-		units[] = {"tfo_baf_crate", "tfo_usa_crate"};
+		units[] = {"tfo_baf_crate", "tfo_usa_crate", "tfo_caf_crate"};
 		weapons[] = {};
 		requiredVersion = 1.32;
 		requiredAddons[] = {};
@@ -33,6 +33,10 @@ class CfgPatches {
 					class virtualAmmoBoxUSA {
 						file = "\TFO_Boxes\scripts\virtualAmmoBoxUSA.sqf";
 						description = "[nameOfObject,type (type number: 0 - all gear, 1 - ammo only)] call tfo_fnc_virtualAmmoBoxUSA, make a object virtual ammo box with american gear inside";
+					};
+					class virtualAmmoBoxCAF {
+						file = "\TFO_Boxes\scripts\virtualAmmoBoxCAF.sqf";
+						description = "[nameOfObject,type (type number: 0 - all gear, 1 - ammo only)] call tfo_fnc_virtualAmmoBoxCAF, make a object virtual ammo box with canadian gear inside";
 					};
 				};
 			};
@@ -69,6 +73,23 @@ class CfgPatches {
 				
 				class EventHandlers {
 					init = "[_this select 0,0] call tfo_fnc_virtualAmmoBoxUSA;";
+				};
+				
+				class TransportWeapons {};
+				
+				class TransportMagazines {};
+				
+				class TransportItems {};
+			};
+			class tfo_usa_crate : Box_NATO_Wps_F {
+				displayName = "[TFO] CSOR Crate (Virtual)";
+				scope = public;
+				icon = "iconCrateLarge";
+				transportMaxWeapons = 40;
+				transportMaxMagazines = 20;
+				
+				class EventHandlers {
+					init = "[_this select 0,0] call tfo_fnc_virtualAmmoBoxCAF;";
 				};
 				
 				class TransportWeapons {};
