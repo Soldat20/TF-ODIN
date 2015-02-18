f4df084c8e1ca9b3c2c4d9162fb9fdce37bbd790
turingware <turingware@users.noreply.github.com>
2015-02-18 09:39:29 -0600

    Create cafconfig.hpp

---
+++ tfo_ammoBoxes/caf/cafconfig.hpp
@@ -0,0 +1,15 @@
+// class CfgVehicles
+// {
+
+	//class B_CargoNet_01_ammo_F;
+	class Box_tfo_caf : B_CargoNet_01_ammo_F {
+		displayname = "[TFO] CAF Box";
+		maximumLoad = 999999;
+		transportmaxmagazines = 999999;
+		transportmaxweapons = 999999;
+		transportMaxBackpacks = 999999;
+		SLX_XEH_DISABLED = 0;
+
+		class TransportItems {
+		// OPTICS
+		
