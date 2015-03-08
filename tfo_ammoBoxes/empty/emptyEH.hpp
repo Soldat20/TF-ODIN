ecccef23c44e86be3a0dc3cb29c6cc4cd4d2da6a
turingware <turingware@users.noreply.github.com>
2015-03-08 14:19:37 -0500

    Update emptyEH.hpp

--- tfo_ammoBoxes/empty/emptyEH.hpp
+++ tfo_ammoBoxes/empty/emptyEH.hpp
@@ -2,7 +2,7 @@
 
 	class Box_tfo_empty
 	{
-		Box_tfo_empty_init = "[_this select 0] execVM '\tfo_ammoBoxes\empty\obj_init.sqf';";
+		Box_tfo_empty_init = "[_this select 0] execVM '\TFO_Test_Boxes\tfo_ammoBoxes\empty\obj_init.sqf';";
 	};
 
 //};
