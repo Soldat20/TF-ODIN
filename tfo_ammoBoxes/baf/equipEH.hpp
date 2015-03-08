8443a2cf078b463449a9ec0dc0f49e93cf981b52
turingware <turingware@users.noreply.github.com>
2015-03-08 14:17:03 -0500

    Update equipEH.hpp

--- tfo_ammoBoxes/baf/equipEH.hpp
+++ tfo_ammoBoxes/baf/equipEH.hpp
@@ -1,6 +1,6 @@
 //class Extended_Init_EventHandlers {
 class Box_tfo_BAF
 {
-Box_tfo_BAF_init = "[_this select 0] execVM '\tfo_ammoBoxes\baf\obj_init.sqf';";
+Box_tfo_BAF_init = "[_this select 0] execVM '\TFO_Test_Boxes\tfo_ammoBoxes\baf\obj_init.sqf';";
 };
 //};
