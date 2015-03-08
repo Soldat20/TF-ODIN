8d169cf82081657d416f1ef92e9292bbca0f87cb
turingware <turingware@users.noreply.github.com>
2015-03-08 14:16:17 -0500

    Update equipEH.hpp

--- tfo_ammoBoxes/USMC/equipEH.hpp
+++ tfo_ammoBoxes/USMC/equipEH.hpp
@@ -1,6 +1,6 @@
 //class Extended_Init_EventHandlers {
 class Box_tfo_USMC
 {
-Box_tfo_USMC_init = "[_this select 0] execVM '\tfo_ammoBoxes\USMC\obj_init.sqf';";
+Box_tfo_USMC_init = "[_this select 0] execVM '\TFO_Test_Boxes\tfo_ammoBoxes\USMC\obj_init.sqf';";
 };
 //};
